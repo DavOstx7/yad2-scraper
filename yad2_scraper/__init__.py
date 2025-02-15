f5251a4b9a83eb8617ea9771cb999c9f5334753a
dav ost <private>
2025-02-15 22:29:21 +0200

    add fetch category helpers to __init__.py

--- yad2_scraper/__init__.py
+++ yad2_scraper/__init__.py
@@ -1,4 +1,58 @@
-from .scraper import Yad2Scraper
-from .query import QueryFilters, OrderBy
+from typing import Optional, Type
+
+from .scraper import Yad2Scraper, Category
+from .query import QueryFilters, OrderBy, NumberRange
 from .category import Yad2Category
 from .next_data import NextData
+from .utils import any_param_specified
+from .vehicles import (
+    Yad2VehiclesCategory,
+    VehiclesQueryFilters,
+    OrderVehiclesBy,
+    VehicleType,
+    get_vehicle_url
+)
+
+_default_scraper = None
+
+
+def get_default_scraper() -> Yad2Scraper:
+    global _default_scraper
+
+    if not _default_scraper:
+        _default_scraper = Yad2Scraper()
+
+    return _default_scraper
+
+
+def fetch_category(
+        url: str,
+        category_type: Type[Category] = Yad2Category,
+        page: Optional[int] = None,
+        order_by: Optional[OrderBy] = None,
+        price_range: [NumberRange] = None
+) -> Category:
+    if any_param_specified(page, order_by, price_range):
+        params = QueryFilters(page=page, order_by=order_by, price_range=price_range)
+    else:
+        params = None
+
+    default_scraper = get_default_scraper()
+    return default_scraper.fetch_category(url, category_type, params=params)
+
+
+def fetch_vehicle_category(
+        vehicle_type: VehicleType,
+        page: Optional[int] = None,
+        order_by: Optional[OrderVehiclesBy] = None,
+        price_range: [NumberRange] = None,
+        year_range: [NumberRange] = None
+) -> Yad2VehiclesCategory:
+    if any_param_specified(page, order_by, price_range, year_range):
+        params = VehiclesQueryFilters(page=page, order_by=order_by, price_range=price_range, year_range=year_range)
+    else:
+        params = None
+
+    vehicle_url = get_vehicle_url(vehicle_type)
+    default_scraper = get_default_scraper()
+    return default_scraper.fetch_category(vehicle_url, Yad2VehiclesCategory, params=params)
